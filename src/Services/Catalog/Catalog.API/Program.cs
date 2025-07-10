a71e72289b869816e13ad1585a1781f5933bd888
Shamil Heydarov <samilheyderov2003@gmail.com>
2025-07-10 14:39:42 +0400

    Extend catalog functionalities

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
-//Add services to the container.
-
+// Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 var app = builder.Build();
 
+app.MapCarter();
 // Configure the HTTP request pipeline.
 
 app.Run();
